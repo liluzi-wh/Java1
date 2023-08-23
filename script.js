d7c8624ae084cf14eff7e7c8594a48abaa34ac91
Rauf <totqwu@gmail.com>
2023-08-23 10:44:59 +0300

    java

---
+++ script.js
@@ -0,0 +1,38 @@
+var im = prompt('введите свое имя');
+
+var year = prompt('введите свой возраст');
+
+var plus = prompt('решите пример:6 + 6');
+
+var minus = prompt('решите пример:20 - 10');
+
+var umnojit = prompt('решите пример:10 * 2');
+
+var del = prompt('решите пример:8 / 4');
+
+var procent = prompt('решите пример:10 % 3');
+
+
+
+
+
+console.log('привет! меня зовут ' + im)
+
+console.log('мой возраст ' + year)
+
+console.log('пример 1: 6 + 6 = 12 Ваш ответ: ' + plus)
+
+console.log('пример 2: 20 - 10 = 10 Ваш ответ: ' + minus)
+
+console.log('пример 3: 10 * 2 = 20 Ваш ответ: ' + umnojit)
+
+console.log('пример 4: 8 / 4 = 2 Ваш ответ: ' + del)
+
+console.log('пример 5: 10 % 3 = 1 Ваш ответ ' + procent)
+
+
+
+
+
+
+var gg = prompt('Тест окончен,проверьте ваши ответы!');
